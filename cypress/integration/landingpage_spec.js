0674d16c71c27cd96a164c6f0111b8b2b640c3d4
Mark <mark.aaron.fisher@gmail.com>
2018-12-13 16:32:26 -0800

    add some new login tests not yet completed

--- cypress/integration/landingpage_spec.js
+++ cypress/integration/landingpage_spec.js
@@ -1,22 +1,48 @@
 
 beforeEach(()=>{
-  cy.on('uncaught:exception', (err, runnable) => {
-  expect(err.message).to.include('of undefined')
-    done()
-    return false
-  });
+
 });
 
 describe('Wildbook instance landing page', function() {
-  it('visits landing page and finds something that says submit!', function() {
+  before(function() {
     cy.visit('/');
+    cy.on('uncaught:exception', (err, runnable) => {
+    expect(err.message).to.include('of undefined')
+      done()
+      return false
+    });
+  });
+  it('visits landing page and finds something that says submit!', function() {
     cy.contains('Submit');
   });
 });
 
+describe('Log in page tests', function(){
+  before(function() {
+    cy.visit('https://www.flukebook.org/login.jsp');
+    cy.on('uncaught:exception', (err, runnable) => {
+    expect(err.message).to.include('of undefined')
+      done()
+      return false
+    });
+  });
+  it('logs in', function(){
+    cy.get('input[name=username]').type('atticus29');
+    cy.get('input[name=password]').type('FPython!11');
+    cy.get('input[name=submit]').click();
+  });
+});
+
 describe('Wildbook instance landing page known bugs', function() {
-  it('looks for text containing null', function() {
+  before(function() {
     cy.visit('/');
+    cy.on('uncaught:exception', (err, runnable) => {
+    expect(err.message).to.include('of undefined')
+      done()
+      return false
+    });
+  });
+  it('looks for text containing null', function() {
     cy.contains('Mark Aaron Fisher').should('not.exist');
     cy.contains('null').should('not.exist');
   });
