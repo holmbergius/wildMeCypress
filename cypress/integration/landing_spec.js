42bd54edd0f9b456e13b7dc1a53eb212b1036026
Mark <mark.aaron.fisher@gmail.com>
2018-12-15 18:45:25 -0800

    add several login specs that should pass but submission of the login form is taking the user to an encounter page and prompting request collaboration with Shane Gero

---
+++ cypress/integration/landing_spec.js
@@ -0,0 +1,19 @@
+describe('Wildbook instance landing page known bugs', function() {
+  before(function() {
+    cy.visit('/logout.jsp');
+    cy.visit('/');
+    cy.on('uncaught:exception', (err, runnable) => {
+    expect(err.message).to.include('of undefined')
+      done()
+      return false
+    });
+  });
+
+  it('should not contain How It Works text', function() {
+    cy.contains('HOW IT WORKS').should('exist');
+  });
+
+  it('should not contain null text', function() {
+    cy.contains('null').should('not.exist');
+  });
+});
--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,54 +1,56 @@
-// beforeEach(()=>{
-//
-// });
-
-describe('Wildbook instance landing page', function() {
+describe('Wildbook instance login page', function() {
+  beforeEach(()=>{
+    cy.visit('/logout.jsp');
+    cy.visit('/login.jsp');
+  });
   before(function() {
-    cy.visit('/');
     cy.on('uncaught:exception', (err, runnable) => {
     expect(err.message).to.include('of undefined')
       done()
       return false
     });
   });
-  it('visits landing page and finds something that says submit!', function() {
-    cy.contains('Submit');
+
+  it('greets with sign in', function() {
+    cy.contains('Database login');
   });
+
+  it('cannot visit welcome page before logging in', function(){
+    cy.visit('/welcome.jsp');
+    cy.contains('Database login').should('exist');
+  });
+
+  it('navigates to welcome.jsp on successful login', function(){
+    cy.get('input[name=username]').type('atticus29'); //TODO put username in a better place
+    cy.get('input[name=password]').type('FPython!11'); //TODO put password in a better place
+    cy.get('form').first().submit(); //TODO brittle if they add more forms but forms don't have ids currently in login.jsp
+    cy.contains('Database login').should('not.exist');
+    cy.contains('Login success').should('exist');
+  });
+
+  it('requires username', function(){
+    cy.get('input[name=username]').type('atticus29'); //TODO put username in a better place
+    cy.get('form').first().submit(); //TODO brittle if they add more forms but forms don't have ids currently in login.jsp
+    cy.contains('Database login').should('exist'); //TODO this currently takes you to an encounter page and then displays Request collaboration with Shane Gero??
+    // cy.get('input[value=Cancel]').click();
+  });
+
+  it('requires password', function(){
+    cy.get('input[name=password]').type('FPython!11'); //TODO put password in a better place
+    cy.get('form').first().submit(); //TODO brittle if they add more forms but forms don't have ids currently in login.jsp
+    cy.contains('Database login').should('exist');
+  });
+
+  it('requires valid username and password', function(){
+    cy.get('input[name=username]').type('atticus2'); //TODO put username in a better place
+    cy.get('input[name=password]').type('FPyth11'); //TODO put password in a better place
+    cy.get('form').first().submit(); //TODO brittle if they add more forms but forms don't have ids currently in login.jsp
+    cy.contains('Database login').should('exist');
+  });
+
 });
 
 describe('Log in page tests', function(){
-  before(function() {
-    cy.visit('/login.jsp');
-    cy.on('uncaught:exception', (err, runnable) => {
-    expect(err.message).to.include('of undefined')
-      done()
-      return false
-    });
-  });
-  it('cannot visit welcome page before logging in', function(){
-    cy.visit('/welcome.jsp');
-    cy.contains('login').should('exist');
-  });
-  it('logs in', function(){
-    cy.get('input[name=username]').type('atticus29');
-    cy.get('input[name=password]').type('FPython!11');
-    cy.get('input[name=submit]').submit(); //TODO figure out why this isn't working
-    cy.visit('/welcome.jsp');
-    cy.contains('login').should('not.exist');
-  });
-});
 
-describe('Wildbook instance landing page known bugs', function() {
-  before(function() {
-    cy.visit('/');
-    cy.on('uncaught:exception', (err, runnable) => {
-    expect(err.message).to.include('of undefined')
-      done()
-      return false
-    });
-  });
-  it('looks for text containing null', function() {
-    cy.contains('Mark Aaron Fisher').should('not.exist');
-    cy.contains('null').should('not.exist');
-  });
+
 });
