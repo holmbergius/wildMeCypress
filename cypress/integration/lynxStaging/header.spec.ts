1b6629f776d48de8410d5f64efbdbbe784067b16
Mark <mark.aaron.fisher@gmail.com>
2020-05-26 22:45:45 -0700

    all new header.jsp tests fail on production lynx and pass on staging lynx, as expected

--- cypress/integration/lynxStaging/header.spec.ts
+++ cypress/integration/lynxStaging/header.spec.ts
@@ -1,6 +1,6 @@
 describe('Tests associated with the header in the lynx repo', function() {
   beforeEach(()=>{
-    Cypress.config('baseUrl', 'https://lynx.wildbook.org/'); //TODO change to https://general-staging.wildbook.org/
+    Cypress.config('baseUrl', 'https://lynx.wildbook.org/'); //production URL: https://lynx.wildbook.org/ https://general-staging.wildbook.org/
     cy.logout();
     cy.fixture('localVariables.json').then((localVars)=>{
       cy.loginLynxStaging(localVars.username, localVars.password);
@@ -10,37 +10,38 @@ describe('Tests associated with the header in the lynx repo', function() {
     cy.logout();
   });
 
-  it.skip('does not see Submit → Create Survey in navbar', function(){
-      cy.contains('a', 'Create Survey').should('not.be.visible');
-      cy.contains('a', 'Submit').click({force: true});
-      cy.contains('a', 'Create Survey').should('not.be.visible');
+  it('does not see Submit → Create Survey in navbar', function(){
+      cy.contains('a', 'Create Survey').should('not.exist');
+      cy.contains('a', 'Submit').should('not.exist');
+      // cy.contains('a', 'Submit').click({force: true});
+      cy.contains('a', 'Create Survey').should('not.exist');
   });
 
   it('does not see Search → Individual search in navbar', function(){
-      cy.get('a[id=search-dropdown]').click({force: true});
-      cy.contains('a', 'Individual Search').should('be.visible'); //TODO change to not.be.visible
+      cy.get('a[id=search-dropdown]').should('not.exist');
+      cy.contains('a', 'Individual Search').should('not.exist');
   });
 
   it('does not see Search → Occurrence search in navbar', function(){
-      cy.get('a[id=search-dropdown]').click({force: true});
-      cy.contains('a', 'Occurrence Search').should('be.visible'); //TODO change to not.be.visible
+      cy.get('a[id=search-dropdown]').should('not.exist');
+      cy.contains('a', 'Occurrence Search').should('not.exist');
   });
 
   it('does not see Search → Survey search in navbar', function(){
-      cy.get('a[id=search-dropdown]').click({force: true});
-      cy.contains('a', 'Survey Search').should('be.visible'); //TODO change to not.be.visible
+      cy.get('a[id=search-dropdown]').should('not.exist');
+      cy.contains('a', 'Survey Search').should('not.exist');
   });
 
-  it.only('does not see Encounters → View Images in navbar', function(){
+  it('does not see Encounters → View Images in navbar', function(){
       cy.contains('a', 'Encounters').should('be.visible');
       cy.contains('a', 'Encounters').click({force: true});
-      cy.contains('a', 'View Images').should('not.be.visible');
+      cy.contains('a', 'View Images').should('not.exist');
   });
 
   it('does not see Ecounters → Encounter Calendar in navbar', function(){
     cy.contains('a', 'Encounters').should('be.visible');
     cy.contains('a', 'Encounters').click({force: true});
-    cy.contains('a', 'Encounter Calendar').should('not.be.visible');
+    cy.contains('a', 'Encounter Calendar').should('not.exist');
   });
 
 });
