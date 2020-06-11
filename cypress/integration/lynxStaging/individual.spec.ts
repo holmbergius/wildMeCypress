c51f43d084eeb5404cb63169c89fe276aa8b0726
Mark <mark.aaron.fisher@gmail.com>
2020-06-10 21:39:23 -0700

    add tests for encounter search when you have collaborators and non-collaborators

--- cypress/integration/lynxStaging/individual.spec.ts
+++ cypress/integration/lynxStaging/individual.spec.ts
@@ -9,7 +9,7 @@ describe('Tests relating to individuals.jsp', function() {
   afterEach(function () {
     cy.logout();
   });
-  it("Shows test individual when navigating to when logged in as admin", function(){
+  it("Shows test individual when navigating to its encounter page when logged in as admin", function(){
     cy.fixture('liveVariables.json').then((liveVars) =>{
       cy.visit("/individuals.jsp?number=" + liveVars.testIndividualName);
       cy.contains('h1', liveVars.markedIndividualWithName).should('exist');
@@ -34,6 +34,35 @@ describe('Tests relating to individuals.jsp', function() {
    cy.fixture('liveVariables.json').then((liveVars) =>{
      cy.visit("/individuals.jsp?number=" + liveVars.testIndividualName);
      cy.contains('a', liveVars.cooccurrencesTableText).should('not.exist');
+   });
+ });
+
+ it("Shows collaborator B being able to see an encounter created by collaborator A", function(){
+   cy.fixture('liveVariables.json').then((liveVars) =>{
+     cy.logout();
+     cy.loginLynxStaging(liveVars.collaboratorB_Username, liveVars.collaboratorB_Password);
+     cy.visit('/encounters/encounter.jsp?number=32017582-7734-43ea-afac-5fd376232475');
+     cy.contains(liveVars.nonCollaboratorMessage).should('not.exist');
+   });
+ });
+
+ it("Shows collaborator C not being able to see an encounter created by collaborator A", function(){
+   cy.fixture('liveVariables.json').then((liveVars) =>{
+     cy.logout();
+     cy.loginLynxStaging(liveVars.collaboratorC_Username, liveVars.collaboratorC_Password);
+     cy.visit('/encounters/encounter.jsp?number=32017582-7734-43ea-afac-5fd376232475');
+     cy.contains(liveVars.nonCollaboratorMessage).should('exist');
+   });
+ });
+
+ it("Shows collaborator C not being able to see search results of encounters created by collaborator A", function(){
+   cy.fixture('liveVariables.json').then((liveVars) =>{
+     cy.logout();
+     cy.loginLynxStaging(liveVars.collaboratorC_Username, liveVars.collaboratorC_Password);
+     cy.visit('/encounters/searchResults.jsp?ne_lat=&ne_long=&sw_lat=&sw_long=&locationField=&datepicker1=&datepicker2=&dateaddedpicker1=&dateaddedpicker2=&male=male&female=female&unknown=unknown&genusField=&alive=alive&dead=dead&observationKey1=&observationValue1=&numSearchedObs=1&lifeStageField=None&numResights=1&alternateIDField=&individualID=&tissueSampleID=&biomeasurement13C%28operator%29=gteq&biomeasurement13C%28value%29=&biomeasurement15N%28operator%29=gteq&biomeasurement15N%28value%29=&biomeasurement34S%28operator%29=gteq&biomeasurement34S%28value%29=&nameField=&additionalCommentsField=&username=collaborator_a&submitSearch=Search+Sightings');
+     // cy.visit('/encounters/searchResults.jsp?ne_lat=&ne_long=&sw_lat=&sw_long=&locationField=&datepicker1=&datepicker2=&dateaddedpicker1=&dateaddedpicker2=&male=male&female=female&unknown=unknown&genusField=&alive=alive&dead=dead&observationKey1=&observationValue1=&numSearchedObs=1&lifeStageField=None&measurementWaterTemperature%28operator%29=gteq&measurementWaterTemperature%28value%29=&measurementSalinity%28operator%29=gteq&measurementSalinity%28value%29=&measurementcarapacewidth%28operator%29=gteq&measurementcarapacewidth%28value%29=&measurementcarapacelength%28operator%29=gteq&measurementcarapacelength%28value%29=&numResights=1&alternateIDField=&individualID=&metalTag%28left%29=&metalTag%28right%29=&acousticTagSerial=&acousticTagId=&satelliteTagName=None&satelliteTagSerial=&satelliteTagArgosPttNumber=&tissueSampleID=&nameField=&additionalCommentsField=&username=collaborator_a&submitSearch=Search+Sightings');
+     cy.contains('Loading results table...').should('not.be.visible');
+     cy.contains('tr',liveVars.collaboratorA_imageName).should('not.exist');
    });
  });
 
